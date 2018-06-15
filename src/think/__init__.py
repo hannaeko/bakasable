74cb6f600254f373635ebbade2cca00b0e4a340b
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-06-15 14:00:09 +0900

    Add think function logic

---
+++ src/think/__init__.py
@@ -0,0 +1,39 @@
+from sortedcontainers import SortedSet
+
+
+class BaseThink:
+    bag = set()
+
+    def __init__(self, **param):
+        self.action_param = param
+
+    def __call__(self, func):
+        self.callback = func
+        self.bag.add(self)
+        return func
+
+    def test(self, param):
+        return all(key in param and isinstance(param[key], value)
+                   for key, value in self.action_param.items())
+
+    @classmethod
+    def execute(cls, param):
+        for event in cls.bag:
+            if event.test(param):
+                event.callback(**param)
+
+
+class on_action(BaseThink):
+    pass
+
+
+class on_event(BaseThink):
+    pass
+
+
+class on_loop(BaseThink):
+    bag = SortedSet(key=lambda e: e.priority)
+
+    def __init__(self, **param):
+        self.priority = param.pop('priority', 500)
+        super().__init__(**param)
