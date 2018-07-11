38f1d8ce50a16364f6c77eaf684057116e315287
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-07-11 17:32:15 +0900

    less nested loops in game rendering

--- bakasable/game/game.py
+++ bakasable/game/game.py
@@ -42,29 +42,29 @@ class Game:
         chunks = filter(None,
                         (self.context.object_store.get_chunk(*chunk_coord)
                          for chunk_coord in chunks_coord))
-        entity_big_bag = set()
+        entity_bag = set()
 
         for chunk in chunks:
             self.screen.blit(
                 chunk.map.current_frame,
                 (chunk.map.x * 15 * TILE_SIZE - top_left_x,
                  chunk.map.y * 15 * TILE_SIZE - top_left_y))
-            entity_big_bag.add(chunk.entities)
-        for entity_small_bag in entity_big_bag:
-            for entity in entity_small_bag:
-                if entity.sprite_name:
-                    entity.sprite.update(self.context.dt)
-                    self.screen.blit(entity.current_frame,
-                                     (entity.x * TILE_SIZE - top_left_x,
-                                      entity.y * TILE_SIZE - top_left_y))
-                try:
-                    for sprite, x_delta, y_delta in entity.additional_sprites:
-                        self.screen.blit(
-                            sprite,
-                            (entity.x * TILE_SIZE - top_left_x + x_delta,
-                             entity.y * TILE_SIZE - top_left_y + y_delta))
-                except AttributeError:
-                    pass
+            entity_bag.update(chunk.entities)
+
+        for entity in entity_bag:
+            if entity.sprite_name:
+                entity.sprite.update(self.context.dt)
+                self.screen.blit(entity.current_frame,
+                                 (entity.x * TILE_SIZE - top_left_x,
+                                  entity.y * TILE_SIZE - top_left_y))
+            try:
+                for sprite, x_delta, y_delta in entity.additional_sprites:
+                    self.screen.blit(
+                        sprite,
+                        (entity.x * TILE_SIZE - top_left_x + x_delta,
+                         entity.y * TILE_SIZE - top_left_y + y_delta))
+            except AttributeError:
+                pass
 
     def process_events(self):
         if not self.context.graphics:
