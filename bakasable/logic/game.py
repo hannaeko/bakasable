7306bfe3dc04dc23dac80d2dbb21158e216b2e24
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-07-11 15:40:29 +0900

    fix player animation trigger

--- bakasable/logic/game.py
+++ bakasable/logic/game.py
@@ -12,7 +12,6 @@ def quit_game(context, **kw):
 @on_event(type=pygame.KEYDOWN)
 def start_moving_player(context, key, **kw):
     player = context.object_store.get(context.peer_id)
-    player.sprite.start_animation()
     if key == pygame.K_UP:
         player.speed.y -= PLAYER_WALKING_SPEED
     if key == pygame.K_DOWN:
@@ -21,6 +20,8 @@ def start_moving_player(context, key, **kw):
         player.speed.x -= PLAYER_WALKING_SPEED
     if key == pygame.K_RIGHT:
         player.speed.x += PLAYER_WALKING_SPEED
+    if player.speed.x != 0 or player.speed.y != 0:
+        player.sprite.start_animation()
 
 
 @on_event(type=pygame.KEYUP)
